5f2b038a8323826a5029e94c8322d243df0333cf
anthony cantor <ar.cantor@gmail.com>
2014-05-17 09:55:13 +0000

    added x86 arch name to system module

--- mozz/system.py
+++ mozz/system.py
@@ -1,6 +1,7 @@
 import platform
 
 ARCH_X86_64 = "x86_64"
+ARCH_X86 = "x86"
 ARCH_UNKNOWN = "unknown"
 
 def architecture():
@@ -8,5 +9,7 @@ def architecture():
 
 	if mach == ARCH_X86_64:
 		return ARCH_X86_64
+	elif mach == ARCH_X86:
+		return ARCH_X86
 	else:
 		return ARCH_UNKNOWN
